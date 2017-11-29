4b380e9294047b841ac4540781351371120b9fd7
Informatica7ITP <informseptimo2017@gmail.com>
2017-11-28 22:47:21 -0600

    Version 1 página web

---
+++ Web_Home_Assistant/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+          <meta charset="utf-8"/>	
+          <meta name=”description” content="Automatización en Home Assistant"> 
+          <meta name=”keywords” content="Broker, Home Assistant, Raspberry Pi">
+
+	<title> Inicio</title>
+	<link rel="stylesheet" href="estilos/principal.css">
+
+</head>
+<body>
+
+<div id="contenedor">
+
+
+    <div id="cab">
+     <div id="cabecera">
+         
+         <div class="logoi">
+             <img src="images/logo1.png">
+         </div>
+         
+         <div id="logocentral">
+          <h1>
+          Home Assistant   
+          </h1>
+         </div>
+         
+         <div class="logod">
+             
+         </div>
+     	
+     </div>
+</div>
+     <ul class="nav">
+				<li><a href="">Inicio</a></li>
+				<li><a href="">Servicios</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a>
+							<ul>
+								<li><a href="">Submenu1</a></li>
+								<li><a href="">Submenu2</a></li>
+								<li><a href="">Submenu3</a></li>
+								<li><a href="">Submenu4</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+				<li><a href="">Acerca de</a>
+					<ul>
+						<li><a href="">Submenu1</a></li>
+						<li><a href="">Submenu2</a></li>
+						<li><a href="">Submenu3</a></li>
+						<li><a href="">Submenu4</a></li>
+					</ul>
+				</li>
+				<li><a href="">Contacto</a></li>
+			</ul>
+		</div>
+
+     <div id="slaid">
+          <img src="images/ban.png">
+
+            <div id="slaider">
+            	
+            </div>
+     	
+     </div>
+	
+
+
+</div>
+
+</body>
+</html>
