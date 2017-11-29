a66a64af9c1c9739863bb3d37fee86377e203b62
Informatica7ITP <informseptimo2017@gmail.com>
2017-11-29 15:33:13 -0600

    Version 2

--- Web_Home_Assistant/index.php
+++ Web_Home_Assistant/index.php
@@ -7,7 +7,19 @@
 
 	<title> Inicio</title>
 	<link rel="stylesheet" href="estilos/principal.css">
+    
+    
+    <link rel="stylesheet" href="css/nivo-slider.css">
+	<link rel="stylesheet" href="css/mi-slider.css">
 
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js" type="text/javascript"></script>
+	<script src="js/jquery.nivo.slider.js"></script>
+
+	<script type="text/javascript"> 
+		$(window).on('load', function() {
+		    $('#slider').nivoSlider(); 
+		}); 
+	</script>
 </head>
 <body>
 
@@ -17,60 +29,90 @@
     <div id="cab">
      <div id="cabecera">
          
-         <div class="logoi">
+         <div class="logoi" id="animacion"  >
              <img src="images/logo1.png">
          </div>
          
-         <div id="logocentral">
+         <div id="logocentral" class="animacion">
           <h1>
           Home Assistant   
           </h1>
          </div>
          
          <div class="logod">
-             
+              <img src="images/itp.png">
          </div>
      	
      </div>
 </div>
+ <div> 
      <ul class="nav">
-				<li><a href="">Inicio</a></li>
-				<li><a href="">Servicios</a>
+				<li><a id="inicio" href="index.php">Inicio</a></li>
+				<li><a href="">Acerca de </a>
 					<ul>
-						<li><a href="">Submenu1</a></li>
+						<li><a href="">Objetivos</a></li>
 						<li><a href="">Submenu2</a></li>
-						<li><a href="">Submenu3</a></li>
-						<li><a href="">Submenu4</a>
-							<ul>
-								<li><a href="">Submenu1</a></li>
-								<li><a href="">Submenu2</a></li>
-								<li><a href="">Submenu3</a></li>
-								<li><a href="">Submenu4</a></li>
-							</ul>
-						</li>
 					</ul>
 				</li>
-				<li><a href="">Acerca de</a>
+				<li><a href="">Documentos</a>
 					<ul>
-						<li><a href="">Submenu1</a></li>
-						<li><a href="">Submenu2</a></li>
-						<li><a href="">Submenu3</a></li>
-						<li><a href="">Submenu4</a></li>
+						<li><a href="">Instalación de Raspbian</a></li>
+						<li><a href="">Configuración de la Red WAN</a>
+                                <ul>
+								<li><a href="">Modo Consola</a></li>
+								<li><a href="">Modo Gráfico</a></li>
+							    </ul>
+                            </li>
+						<li><a href="">Instalación de Home Assistant </a></li>
+						<li><a href="">Intregacion de modulo relay</a></li>
+                        
 					</ul>
 				</li>
-				<li><a href="">Contacto</a></li>
+				<li><a href="">Tutoriales</a></li>
+                <li><a href="">Recursos</a></li>
+                <li><a href="">Ayuda</a></li>
 			</ul>
-		</div>
-
+                
+    </div>
+		
+<div>
      <div id="slaid">
-          <img src="images/ban.png">
+     
+     <div class="slider-wrapper theme-mi-slider">
+		<div id="slider" class="nivoSlider">     
+		    <img src="img/1.jpg" alt="" title="#htmlcaption1" />    
+		    <img src="img/2.jpg" alt="" title="#htmlcaption2" />    
+		    <img src="img/3.jpg" alt="" title="#htmlcaption3" />     
+		</div> 
+		<div id="htmlcaption1" class="nivo-html-caption">     
+		    <h3> </h3>
+		    <p> </p>
+		</div>
+		<div id="htmlcaption2" class="nivo-html-caption">     
+		    <h3> </h3>
+		</div>
+		<div id="htmlcaption3" class="nivo-html-caption">     
+		    <h3> </h3>
+		</div>
+	</div>
+    </div>
+    
 
-            <div id="slaider">
-            	
-            </div>
-     	
-     </div>
-	
+</div>         
+    <div id="Home">
+ 
+    <h2>
+        Home Assistant
+    </h2>
+ 
+        <a href="Home Assitant.php">
+        <div id="leer">
+        <p>
+        Leer Más
+        </p>
+        </div>
+            </a>
+    </div>	
 
 
 </div>
